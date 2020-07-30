8fd9f6ab080febd3ddafabc0abc56c028dbeaf7b
Njeri Cooper <njericooper@gmail.com>
2020-07-29 20:22:49 -0400

    fixed form 1 css

--- wip/form.js
+++ wip/form.js
@@ -14,8 +14,7 @@ function setBubble(range, bubble) {
   const min = range.min ? range.min : 0;
   const max = range.max ? range.max : 100;
   const newVal = Number(((val - min) * 100) / (max - min));
-  bubble.innerHTML = val;
+  bubble.innerHTML = "$" + val;
 
-  // Sorta magic numbers based on size of the native UI thumb
   bubble.style.left = `calc(${newVal}% + (${8 - newVal * 0.15}px))`;
 }
